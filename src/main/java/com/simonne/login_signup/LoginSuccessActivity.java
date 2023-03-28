cabd717c7c1186068709a4b9afe7be3527d74f5b
rahuls1307 <92785247+rahuls1307@users.noreply.github.com>
2023-03-28 16:10:04 +0530

    Add files via upload

---
+++ src/main/java/com/simonne/login_signup/LoginSuccessActivity.java
@@ -0,0 +1,27 @@
+package com.simonne.login_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginSuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_success);
+
+        TextView text = (TextView)findViewById(R.id.loginSuccessful);
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            text.setText("Login Successful. Details== Name:" + bundle.getString("name") );
+        }
+    }
+
+    public void logout(View view) {
+        finish();
+    }
+}
